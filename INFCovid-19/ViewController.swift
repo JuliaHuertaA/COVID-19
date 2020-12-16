d5a42fc359b9bae397aeff91cb289b05c995e4d0
Luis Eduardo Perez Monzalvo <theluiseduard@gmail.com>
2020-12-16 02:05:58 -0600

    Elaboración de interfaces

---
+++ INFCovid-19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  INFCovid-19
+//
+//  Created by Mac4 on 16/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
